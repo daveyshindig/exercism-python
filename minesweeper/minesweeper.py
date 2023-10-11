937f9c6fa2fcdf343282fdf23c75080fda759aab
daveyshindig <davey.wilkie@gmail.com>
2023-10-10 14:56:12 -1000

    Python Minesweeper WIP

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,3 +1,12 @@
+def check_board(minefield):
+    for x in minefield:
+        if not len(x) == len(minefield[0]):
+            raise ValueError("The board is invalid with current input.")
+        for ch in x:
+            if ch != ' ' and ch != '*':
+                raise ValueError("The board is invalid with current input.")
+
+
 def count_neighbor_mines(minefield, row_number, column_number):
     mines = 0
     if minefield[row_number][column_number] == "*":
@@ -7,10 +16,18 @@ def count_neighbor_mines(minefield, row_number, column_number):
             if i >= 0 and i < len(minefield) and j >= 0 and j < len(minefield[0]) \
                       and minefield[i][j] == "*":
                 mines += 1 
-    return str(mines)
+    return str(mines) if mines != 0 else " "
 
 
 def annotate(minefield):
-    rows = range(0, len(minefield))
-    cols = range(0, len(minefield[0]))
-    return [[count_neighbor_mines(minefield, i, j) for j in rows] for i in cols]
+    if minefield == [] or minefield == ['']:
+        return minefield
+    check_board(minefield)
+    rows = []
+    for i, row in enumerate(minefield):
+        cols = ''
+        for j in row:
+            cols += count_neighbor_mines(minefield, i, j)
+        rows + cols
+    
+    return rows
